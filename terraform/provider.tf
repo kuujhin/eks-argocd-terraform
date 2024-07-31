d1ec4e4b416658bea4303604fd7b25c1153daaff
kuujhin <kdoubleu0711@gmail.com>
2024-07-31 10:17:48 +0900

    Initial commit

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# terraform {
+#   required_version = "1.5.7"
+#   backend "s3" {
+#     bucket = "your-bucket-name"
+#     key    = "your-bucket-key"
+#     region = "eu-west-1"
+#   }
+# }
+
