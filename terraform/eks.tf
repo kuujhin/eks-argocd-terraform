eca238f7f75fcb113c727db72da6ab06ace05d93
kuujhin <kdoubleu0711@gmail.com>
2024-08-06 13:54:30 +0900

    add: worker node group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -29,6 +29,24 @@ resource "aws_eks_node_group" "main" {
  }
 }
 
+resource "aws_eks_node_group" "worker" {
+ cluster_name    = aws_eks_cluster.main.name
+ node_group_name = "worker-node-group"
+ node_role_arn   = aws_iam_role.eks_node_role.arn
+ subnet_ids      = aws_subnet.public_subnet.*.id
+
+ scaling_config {
+   desired_size = 2
+   max_size     = 3
+   min_size     = 1
+ }
+
+ instance_types = [ "t3.medium" ]
+
+ tags = {
+   Name = "worker-node-group"
+ }
+}
 
 # resource "kubernetes_namespace" "example" {
 #   metadata {
